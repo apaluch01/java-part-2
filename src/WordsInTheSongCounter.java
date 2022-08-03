7ac94dcf04d286452f49a9337a37545e7f78c8d2
apaluch <apaluch@griddynamics.com>
2022-08-03 10:19:12 +0200

    finished first implementation of the task

--- src/WordsInTheSongCounter.java
+++ src/WordsInTheSongCounter.java
@@ -1,4 +1,60 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class wordsInTheSongCounter {
+public class WordsInTheSongCounter {
+    static String[] songToArray() {
+        String song = "In the town where I was born\n" +
+                "Lived a man who sailed to sea\n" +
+                "And he told us of his life\n" +
+                "In the land of submarines\n" +
+                "So we sailed on to the sun\n" +
+                "'Til we found a sea of green\n" +
+                "And we lived beneath the waves\n" +
+                "In our yellow submarine\n" +
+                "We all live in a yellow submarine\n" +
+                "Yellow submarine, yellow submarine\n" +
+                "We all live in a yellow submarine\n" +
+                "Yellow submarine, yellow submarine\n" +
+                "And our friends are all aboard\n" +
+                "Many more of them live next door\n" +
+                "And the band begins to play\n" +
+                "We all live in a yellow submarine\n" +
+                "Yellow submarine, yellow submarine\n" +
+                "We all live in a yellow submarine\n" +
+                "Yellow submarine, yellow submarine\n" +
+                "Full steam ahead, Mister Boatswain, full steam ahead\n" +
+                "Full steam ahead it is, Sergeant\n" +
+                "(Cut the cable, drop the cable)\n" +
+                "Aye-aye, sir, aye-aye\n" +
+                "Captain, captain\n" +
+                "As we live a life of ease (a life of ease)\n" +
+                "Every one of us (every one of us)\n" +
+                "Has all we need (has all we need)\n" +
+                "Sky of blue (sky of blue)\n" +
+                "And sea of green (sea of green)\n" +
+                "In our yellow (in our yellow)\n" +
+                "Submarine (submarine, aha)\n" +
+                "We all live in a yellow submarine\n" +
+                "A yellow submarine, yellow submarine\n" +
+                "We all live in a yellow submarine\n" +
+                "A yellow submarine, yellow submarine\n" +
+                "We all live in a yellow submarine\n" +
+                "Yellow submarine, yellow submarine\n" +
+                "We all live in a yellow submarine\n" +
+                "Yellow submarine, yellow submarine";
+        String songLyrics = song.replace("\n", " ").replace(",", "").toLowerCase();
+        String[] lyricsArray = songLyrics.split(" ");
+        return lyricsArray;
+    }
+    public static void main(String[] args){
+        String[] lyrics = songToArray();
+        Scanner input = new Scanner(System.in);
+        String word = input.nextLine();//exceptions for input space etc
+        int wordCount = 0;
+        for (int i = 0; i < lyrics.length; i++){
+            if (lyrics[i].equals(word)){
+                wordCount++;
+            }
+        }
+        System.out.println(wordCount);
+    }
 }
